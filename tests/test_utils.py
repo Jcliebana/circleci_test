56dce1b1d2ebfec00ac7761d504fe52d83999166
jcliebanaf <jcliebanaf@gmail.com>
2021-04-29 12:24:20 +0200

    Añadimos archivo python

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,10 @@
 """
 Unit tests for the utils library
 """
-import os,sys,inspect
+import os, sys, inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir) 
+sys.path.insert(0, parent_dir)
 import utils
 
 
