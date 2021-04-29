613eca72c1325a1d8d4afe438de3e741fa2982b0
jcliebanaf <jcliebanaf@gmail.com>
2021-04-29 10:33:59 +0200

    Añadimos archivo python

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+"""
+Unit tests for the utils library
+"""
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
+import utils
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == utils.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == utils.subtract(4, 2)
